9de0289463a86ce11657bb307dd6aad7de41d471
ssac17 <ghkdgksmf13@naver.com>
2023-12-10 11:19:40 +0900

    project create

---
+++ src/main/java/com/study/StudygoApplication.java
@@ -0,0 +1,13 @@
+package com.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudygoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudygoApplication.class, args);
+    }
+
+}
