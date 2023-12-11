476107f865c1cd4da03651ac77c3f3561aa78e3e
ssac17 <ghkdgksmf13@naver.com>
2023-12-11 21:27:08 +0900

    password encoder

--- src/main/java/com/study/account/AccountService.java
+++ src/main/java/com/study/account/AccountService.java
@@ -4,6 +4,7 @@ import com.study.domain.Account;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,6 +14,7 @@ public class AccountService {
 
     private final AccountRepository accountRepository;
     private final JavaMailSender javaMailSender;
+    private final PasswordEncoder passwordEncoder;
 
     public void processNewAccount(SignUpForm signUpForm) {
         Account newAccount = saveNewAccout(signUpForm);
@@ -21,10 +23,11 @@ public class AccountService {
     }
 
     public Account saveNewAccout(SignUpForm signUpForm) {
+
         Account account = Account.builder()
                 .email(signUpForm.getEmail())
                 .nickname(signUpForm.getNickname())
-                .password(signUpForm.getPassword())
+                .password(passwordEncoder.encode(signUpForm.getPassword()))
                 .studyEnrollmentResultByWeb(true)
                 .studyCreateByWeb(true)
                 .studyUpdateByWeb(true)
--- src/test/java/com/study/account/AccountServiceTest.java
+++ src/test/java/com/study/account/AccountServiceTest.java
@@ -1,5 +1,6 @@
 package com.study.account;
 
+import com.study.domain.Account;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,18 +11,19 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.then;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-
-
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class AccountControllerTest {
+class AccountServiceTest {
+
+
 
     @Autowired
     private MockMvc mockMvc;
@@ -29,38 +31,24 @@ class AccountControllerTest {
     @Autowired
     private AccountRepository accountRepository;
 
-    /*
-    * @MockBean
-    * Mockito는 자바에서 사용되는 유닛 테스트를 위한 mock 프레임워크,
-    * Spring 테스트 컨텍스트 내에서 특정 빈(bean)을 Mockito mock으로 대체하도록 지정하는 역할
-    * */
     @MockBean
     private JavaMailSender javaMailSender;
-
-    @Test
-    @DisplayName("회원 가입 처리 - 입력값 오류")
-    void signUpSubmitTest_withWorngInput() throws Exception{
-        mockMvc.perform(post("/sign-up")
-                .param("nickname", "sky")
-                .param("email", "ghkdkgksmf!.ad")
-                .param("password", "1dd"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("account/sign-up"));
-
-    }
 
     @Test
     @DisplayName("회원 가입 처리 - 입력값 정상")
     void signUpSubmitTest_withCorrectInput() throws Exception{
         String mail = "ghkdkgksmf13@naver.com";
+        String password = "1dd3sd3td";
         mockMvc.perform(post("/sign-up")
                         .param("nickname", "sky")
                         .param("email", mail)
-                        .param("password", "1dd3sd3td"))
+                        .param("password", password)
+                        .with(csrf()))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/"));
-
-        assertTrue(accountRepository.existsByEmail(mail));
+        Account account = accountRepository.findByEmail(mail);
+        assertNotNull(account);
+        assertNotEquals(account.getPassword(), password);
         then(javaMailSender).should().send(any(SimpleMailMessage.class));
 
     }
